965ee5c65004ca0cb626286285f8dd3f3275052f
Aryan870 <pawararyan870@gmail.com>
2026-06-05 18:18:55 +0530

    Setup the project and created the sql code and entities

---
+++ src/main/java/com/MoshPrac/Store/entities/Profile.java
@@ -0,0 +1,36 @@
+package com.MoshPrac.Store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "profiles")
+public class Profile {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "loyalty_points")
+    private Integer loyaltyPoints;
+
+}
---
+++ src/main/java/com/MoshPrac/Store/entities/Tag.java
@@ -0,0 +1,23 @@
+package com.MoshPrac.Store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "tags")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+}
